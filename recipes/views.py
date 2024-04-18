9f05c3d1f908968a1776abc52dea29454f9ca665
redifo <kaanredif@gmail.com>
2024-04-18 21:32:46 +0200

    Add create ingredient  view

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import Recipe
 from .serializers import RecipeSerializer
+from .serializers import IngredientSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 class RecipeList(APIView):
@@ -44,3 +45,13 @@ class RecipeDetail(APIView):
         recipe = self.get_object(pk)
         recipe.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CreateIngredient(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = IngredientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
