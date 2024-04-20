6a1ed538c62141d15d8a419bd944dd5a15fc66f7
redifo <kaanredif@gmail.com>
2024-04-20 21:19:44 +0200

    Add recipe detail view tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -44,3 +44,61 @@ class RecipeListViewTests(APITestCase):
         }, format='json')
         
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class RecipeDetailViewTests(APITestCase):
+
+    def create_user_and_recipe(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        recipe = Recipe.objects.create(
+            user=user, 
+            title='Owned Recipe', 
+            description='Sample', 
+            ingredients='Ingredients',
+            preparation_time=10, 
+            cooking_time=20, 
+            servings=2,
+            instructions='Some instructions'
+        )
+        return user, recipe
+
+    def test_user_can_retrieve_recipe_with_valid_id(self):
+        user, recipe = self.create_user_and_recipe()
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(f'/recipes/{recipe.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Owned Recipe')
+
+    def test_user_cannot_retrieve_recipe_with_invalid_id(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/recipes/999/')  
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_recipe_they_own(self):
+        user, recipe = self.create_user_and_recipe()
+        self.client.login(username='testuser', password='12345')
+        response = self.client.put(f'/recipes/{recipe.id}/', {
+            'title': 'Updated Recipe',
+            'description': 'Updated', 
+            'ingredients': 'Updated Ingredients',
+            'preparation_time': 11,
+            'cooking_time': 21,
+            'servings': 3,
+            'instructions': 'Updated instructions'
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, 'Updated Recipe')
+
+    def test_user_cannot_update_recipe_they_dont_own(self):
+        
+        owner_user, recipe = self.create_user_and_recipe()
+        
+        other_user = User.objects.create_user(username='notowner', password='12345')
+        self.client.login(username='notowner', password='12345')
+        response = self.client.put(f'/recipes/{recipe.id}/', {
+            'title': 'Illegally Updated Recipe',
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        recipe.refresh_from_db()
+        self.assertNotEqual(recipe.title, 'Illegally Updated Recipe')
+
