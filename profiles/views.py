add3d541c7df1a735f739b19742f36c16d51f312
redifo <kaanredif@gmail.com>
2024-04-21 14:20:02 +0200

    Refactored Profile and Recipe views in Django app to use generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,24 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request':request})
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            self.check_object_permissions(self.request, Profile.objects.get(pk=pk))
-            return Profile.objects.get(pk=pk)
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request':request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request':request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-   
+    def get_object(self):
+        obj = super().get_object()
+        self.check_object_permissions(self.request, obj)
+        return obj
