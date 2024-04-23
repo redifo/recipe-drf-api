e4e350e1404eb8350209e7e647ba8721522568b4
redifo <kaanredif@gmail.com>
2024-04-23 10:27:49 +0200

    Updated settings in DRF project and removed unnecessary files in frontend

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
@@ -106,17 +106,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
+
+CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  
-    'http://127.0.0.1:3000',  
-    'http://localhost:8000',  
-    'http://127.0.0.1:8000',  
-    ]
+        ]
 
 CORS_ALLOW_CREDENTIALS = True
 
